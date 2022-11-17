ea2c1b69f4a184cf042c55dd73527bdaf0f02024
victorien <deadvicmangame@gmail.com>
2022-11-17 15:55:17 +0100

    exo 51 operations js

---
+++ script.js
@@ -0,0 +1,17 @@
+let nb1 = 5;
+let nb2 = 6;
+
+let result = nb1 + nb2;
+
+let addition;
+addition = result + 2;
+
+let soustraction;
+soustraction = result -1;
+
+alert(result*addition);
+alert(result,soustraction);
+
+let modulo;
+modulo = 5 % 2;
+alert(5 % 2);
